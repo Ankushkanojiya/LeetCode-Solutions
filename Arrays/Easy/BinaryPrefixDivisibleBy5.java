42b4a3c6c2525278a3080d9318e08778cd7b7859
Code with Ankush <43909258+Ankushkanojiya@users.noreply.github.com>
2025-11-24 21:48:49 +0530

    1018. Binary Prefix Divisible By 5

---
+++ Arrays/Easy/BinaryPrefixDivisibleBy5.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryPrefixDivisibleBy5 {
+    public static void main(String[] args) {
+        int[] nums={1,1,1};
+        System.out.println(prefixesDivBy5(nums));
+    }
+    public static List<Boolean> prefixesDivBy5(int[] nums){
+        List<Boolean> answers=new ArrayList<>();
+        int n=nums.length;
+        int number=0;
+        for (int num : nums) {
+            number=(number * 2 + num) % 5;
+            answers.add(number == 0);
+        }
+        return answers;
+    }
+}
